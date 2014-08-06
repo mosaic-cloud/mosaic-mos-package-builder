c2976b72047a72d347a6037dcb1b86d8e82ea02d
Ciprian Dorin Craciun <ciprian@volution.ro>
2014-08-06 22:23:43 +0300

    Minor enhancements to the wrapper

--- sources/mos-package-builder-wrapper.py
+++ sources/mos-package-builder-wrapper.py
@@ -3,14 +3,31 @@
 if __name__ != "__main__" : raise Exception ("expected-main")
 
 import os
+import os.path as path
 import sys
 import urllib
+import uuid
+
+if os.environ.get ("JENKINS_URL") is not None and os.environ.get ("BUILD_TAG") is not None :
+	_workbench = os.environ["WORKSPACE"]
+	_temporary = path.join (os.environ.get ("TMPDIR", "/tmp"), "mosaic-mos-package-builder/temporary", os.environ["BUILD_TAG"])
+else :
+	_workbench = path.join (os.environ.get ("TMPDIR", "/tmp"), "mosaic-mos-package-builder/workbench")
+	_temporary = path.join (os.environ.get ("TMPDIR", "/tmp"), "mosaic-mos-package-builder/temporary", uuid.uuid4 () .hex)
+
+if not path.exists (_temporary) :
+	os.makedirs (_temporary)
+
+_sources_suffixes = [".zip", ".tar", ".cpio"]
+for _sources_suffix in _sources_suffixes :
+	_sources = path.join (_workbench, "sources" + _sources_suffix)
+	if path.exists (_sources) :
+		break
+	_sources = None
+
+_package = path.join (_workbench, "package.rpm")
 
 _script = "http://data.volution.ro/ciprian/public/mosaic/tools/mos-package-builder.py"
-_workbench = os.environ["WORKSPACE"]
-_sources = "%s/sources.zip" % (_workbench,)
-_package = "%s/package.rpm" % (_workbench,)
-_temporary = "/tmp/mosaic-mos-package-builder/temporary/%s" % (os.environ["BUILD_TAG"],)
 
 _configuration = {
 		
@@ -25,7 +42,7 @@ _configuration = {
 		"package-distribution" : os.environ.get ("PackageDistribution"),
 }
 
-_script, _headers = urllib.urlretrieve (_script, "./builder.py")
+_script, _headers = urllib.urlretrieve (_script, path.join (_temporary, "builder.py"))
 _script = open (_script)
 
 _globals = {
